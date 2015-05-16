0833d1ba0a8904c950fe649b9243d2da26630e27
Benjamin <benjamin.beganovic33@gmail.com>
2015-05-16 17:43:48 +0200

    reimenovanje projekta

--- BOM/src/main/java/ba/unsa/etf/si/projekt/Frejmovi/Zaposlenik.java
+++ BOM/src/main/java/ba/unsa/etf/si/projekt/Frejmovi/Zaposlenik.java
@@ -58,15 +58,16 @@ public class Zaposlenik {
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
-				
-				//ovaj try sluzi za postavljanje izgleda aplikacije slicnijeg platformi
-				try { 
-				    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+
+				// ovaj try sluzi za postavljanje izgleda aplikacije slicnijeg
+				// platformi
+				try {
+					UIManager.setLookAndFeel(UIManager
+							.getSystemLookAndFeelClassName());
 				} catch (Exception e) {
-				    e.printStackTrace();
+					e.printStackTrace();
 				}
-				
-				
+
 				try {
 					Zaposlenik window = new Zaposlenik();
 					window.frame.setVisible(true);
@@ -76,8 +77,8 @@ public class Zaposlenik {
 			}
 		});
 	}
-	public void setVisible(boolean visible)
-	{
+
+	public void setVisible(boolean visible) {
 		frame.setVisible(visible);
 	}
 
@@ -98,207 +99,218 @@ public class Zaposlenik {
 		frame.setBounds(100, 100, 713, 665);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-		
+
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		tabbedPane.setBounds(12, 51, 683, 569);
 		frame.getContentPane().add(tabbedPane);
-		
+
 		JPanel panel = new JPanel();
 		tabbedPane.addTab("Sastavnice", null, panel, null);
 		panel.setLayout(null);
-		
+
 		JPanel panel_16 = new JPanel();
 		panel_16.setLayout(null);
 		panel_16.setBorder(BorderFactory.createTitledBorder("Pretraga"));
 		panel_16.setBounds(12, 13, 374, 203);
 		panel.add(panel_16);
-		
+
 		JButton button_11 = new JButton("Pretra\u017Ei");
 		button_11.setBounds(215, 165, 91, 25);
 		panel_16.add(button_11);
-		
+
 		JButton button_12 = new JButton("Prika\u017Ei sve");
 		button_12.setBounds(106, 165, 97, 25);
 		panel_16.add(button_12);
-		
+
 		JLabel label_4 = new JLabel("Pretra\u017Ei po:");
 		label_4.setHorizontalAlignment(SwingConstants.RIGHT);
 		label_4.setBounds(26, 72, 74, 16);
 		panel_16.add(label_4);
-		
+
 		JComboBox comboBox_4 = new JComboBox();
 		comboBox_4.setBounds(106, 34, 200, 22);
 		panel_16.add(comboBox_4);
-		
+
 		textField_3 = new JTextField();
 		textField_3.setColumns(10);
 		textField_3.setBounds(106, 104, 200, 22);
 		panel_16.add(textField_3);
-		
+
 		JComboBox comboBox_5 = new JComboBox();
 		comboBox_5.setBounds(106, 69, 200, 22);
 		panel_16.add(comboBox_5);
-		
+
 		JLabel label_5 = new JLabel("Sortiraj po:");
 		label_5.setBounds(34, 37, 66, 16);
 		panel_16.add(label_5);
-		
+
 		JPanel panel_17 = new JPanel();
 		panel_17.setLayout(null);
 		panel_17.setBorder(BorderFactory.createTitledBorder("Nova sastavnica"));
 		panel_17.setBounds(398, 13, 268, 83);
 		panel.add(panel_17);
-		
+
 		JButton button_13 = new JButton("Kreiraj");
 		button_13.setBounds(159, 45, 97, 25);
 		panel_17.add(button_13);
-		
+
 		JPanel panel_18 = new JPanel();
 		panel_18.setLayout(null);
-		panel_18.setBorder(BorderFactory.createTitledBorder("Postojeća sastavnica"));
+		panel_18.setBorder(BorderFactory
+				.createTitledBorder("Postojeća sastavnica"));
 		panel_18.setBounds(398, 109, 268, 107);
 		panel.add(panel_18);
-		
+
 		JButton button_14 = new JButton("Modifikuj");
 		button_14.setBounds(159, 69, 97, 25);
 		panel_18.add(button_14);
-		
+
 		JButton button_15 = new JButton("Obri\u0161i");
 		button_15.setBounds(12, 69, 97, 25);
 		panel_18.add(button_15);
-		
+
 		JButton button_16 = new JButton("Pregledaj");
 		button_16.setBounds(159, 31, 97, 25);
 		panel_18.add(button_16);
-		
+
 		JPanel panel_19 = new JPanel();
 		panel_19.setBounds(12, 229, 654, 268);
-		panel_19.setBorder(BorderFactory.createTitledBorder("Pregled/odabir sastavnice"));
+		panel_19.setBorder(BorderFactory
+				.createTitledBorder("Pregled/odabir sastavnice"));
 		panel.add(panel_19);
 		panel_19.setLayout(null);
-		
+
 		JScrollPane scrollPane_2 = new JScrollPane();
-		scrollPane_2.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-		scrollPane_2.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		scrollPane_2
+				.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		scrollPane_2
+				.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 		scrollPane_2.setBounds(12, 26, 630, 229);
 		panel_19.add(scrollPane_2);
-		
-		
-		
-		Object rowDataSastavnice[][] = { { "", "", "", "", ""},
-                { "", "", "", "", ""},{ "", "", "", "", ""},
-                { "", "", "", "", ""},{ "", "", "", "", ""},
-                { "", "", "", "", ""},{ "", "", "", "", ""},
-                { "", "", "", "", ""},{ "", "", "", "", ""},
-                { "", "", "", "", ""},{ "", "", "", "", ""},
-                { "", "", "", "", ""} };
-		Object columnNamesSastavnice[] = { "ID", "Naziv", "Cijena", "Trajanje", "Kreirana"};
-		
-		table_2 = new JTable(rowDataSastavnice, columnNamesSastavnice);//sastavnice
+
+		Object rowDataSastavnice[][] = { { "", "", "", "", "" },
+				{ "", "", "", "", "" }, { "", "", "", "", "" },
+				{ "", "", "", "", "" }, { "", "", "", "", "" },
+				{ "", "", "", "", "" }, { "", "", "", "", "" },
+				{ "", "", "", "", "" }, { "", "", "", "", "" },
+				{ "", "", "", "", "" }, { "", "", "", "", "" },
+				{ "", "", "", "", "" } };
+		Object columnNamesSastavnice[] = { "ID", "Naziv", "Cijena", "Trajanje",
+				"Kreirana" };
+
+		table_2 = new JTable(rowDataSastavnice, columnNamesSastavnice);// sastavnice
 		scrollPane_2.setViewportView(table_2);
-		
-		JLabel lblNisteOdabraliNiti_3 = new JLabel("Niste odabrali niti jednu sastavnicu.");
+
+		JLabel lblNisteOdabraliNiti_3 = new JLabel(
+				"Niste odabrali niti jednu sastavnicu.");
 		lblNisteOdabraliNiti_3.setBounds(22, 510, 644, 16);
 		panel.add(lblNisteOdabraliNiti_3);
-		
-		
-		//__
-		Object rowDataProizvodi[][] = { { "", "", "", "", ""},{ "", "", "", "", ""},{ "", "", "", "", ""},
-				{ "", "", "", "", ""},{ "", "", "", "", ""},{ "", "", "", "", ""},{ "", "", "", "", ""}};
-		Object columnNamesProizvodi[] = { "ID", "Serijski broj", "Naziv", "Količina", "Datum nabavke"};
-		
+
+		// __
+		Object rowDataProizvodi[][] = { { "", "", "", "", "" },
+				{ "", "", "", "", "" }, { "", "", "", "", "" },
+				{ "", "", "", "", "" }, { "", "", "", "", "" },
+				{ "", "", "", "", "" }, { "", "", "", "", "" } };
+		Object columnNamesProizvodi[] = { "ID", "Serijski broj", "Naziv",
+				"Količina", "Datum nabavke" };
+
 		JPanel panel_1 = new JPanel();
 		tabbedPane.addTab("Skladište", null, panel_1, null);
 		panel_1.setLayout(null);
-		
+
 		JPanel panel_11 = new JPanel();
 		panel_11.setBounds(12, 229, 654, 268);
-		panel_11.setBorder(BorderFactory.createTitledBorder("Pregled/odabir materijala/poluproizvoda"));
+		panel_11.setBorder(BorderFactory
+				.createTitledBorder("Pregled/odabir materijala/poluproizvoda"));
 		panel_1.add(panel_11);
 		panel_11.setLayout(null);
-		
+
 		JScrollPane scrollPane_3 = new JScrollPane();
-		scrollPane_3.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-		scrollPane_3.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		scrollPane_3
+				.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		scrollPane_3
+				.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 		scrollPane_3.setBounds(12, 26, 630, 229);
 		panel_11.add(scrollPane_3);
 		table_3 = new JTable(rowDataProizvodi, columnNamesProizvodi);
 		scrollPane_3.setViewportView(table_3);
-		
-		JLabel lblNisteOdabraliNiti_1 = new JLabel("Niste odabrali niti jedan materijal/poluproizvod");
+
+		JLabel lblNisteOdabraliNiti_1 = new JLabel(
+				"Niste odabrali niti jedan materijal/poluproizvod");
 		lblNisteOdabraliNiti_1.setBounds(22, 510, 644, 16);
 		panel_1.add(lblNisteOdabraliNiti_1);
-		
+
 		JPanel panel_12 = new JPanel();
 		panel_12.setLayout(null);
 		panel_12.setBorder(BorderFactory.createTitledBorder("Pretraga"));
 		panel_12.setBounds(12, 13, 374, 203);
 		panel_1.add(panel_12);
-		
+
 		JButton button = new JButton("Pretra\u017Ei");
 		button.setBounds(215, 165, 91, 25);
 		panel_12.add(button);
-		
+
 		JButton button_1 = new JButton("Prika\u017Ei sve");
 		button_1.setBounds(106, 165, 97, 25);
 		panel_12.add(button_1);
-		
+
 		JLabel label = new JLabel("Pretra\u017Ei po:");
 		label.setHorizontalAlignment(SwingConstants.RIGHT);
 		label.setBounds(26, 72, 74, 16);
 		panel_12.add(label);
-		
+
 		JComboBox comboBox_2 = new JComboBox();
 		comboBox_2.setBounds(106, 34, 200, 22);
 		panel_12.add(comboBox_2);
-		
+
 		textField_1 = new JTextField();
 		textField_1.setColumns(10);
 		textField_1.setBounds(106, 104, 200, 22);
 		panel_12.add(textField_1);
-		
+
 		JComboBox comboBox_3 = new JComboBox();
 		comboBox_3.setBounds(106, 69, 200, 22);
 		panel_12.add(comboBox_3);
-		
+
 		JLabel label_1 = new JLabel("Sortiraj po:");
 		label_1.setBounds(34, 37, 66, 16);
 		panel_12.add(label_1);
-		
+
 		JPanel panel_13 = new JPanel();
 		panel_13.setLayout(null);
-		panel_13.setBorder(BorderFactory.createTitledBorder("Novi materijal/poluproizvod"));
+		panel_13.setBorder(BorderFactory
+				.createTitledBorder("Novi materijal/poluproizvod"));
 		panel_13.setBounds(398, 13, 268, 83);
 		panel_1.add(panel_13);
-		
+
 		JButton btnUnesi = new JButton("Unesi");
 		btnUnesi.setBounds(159, 45, 97, 25);
 		panel_13.add(btnUnesi);
-		
+
 		JPanel panel_14 = new JPanel();
 		panel_14.setLayout(null);
-		panel_14.setBorder(BorderFactory.createTitledBorder("Postojeći materijal/poluproizvod"));
+		panel_14.setBorder(BorderFactory
+				.createTitledBorder("Postojeći materijal/poluproizvod"));
 		panel_14.setBounds(398, 109, 268, 107);
 		panel_1.add(panel_14);
-		
+
 		JButton button_3 = new JButton("Modifikuj");
 		button_3.setBounds(159, 69, 97, 25);
 		panel_14.add(button_3);
-		
+
 		JButton button_4 = new JButton("Obri\u0161i");
 		button_4.setBounds(12, 69, 97, 25);
 		panel_14.add(button_4);
-		
+
 		JButton btnPregledaj = new JButton("Pregledaj");
 		btnPregledaj.setBounds(159, 31, 97, 25);
 		panel_14.add(btnPregledaj);
-		
+
 		JButton btnPretrai = new JButton("Pretra\u017Ei");
 		btnPretrai.setBounds(215, 165, 91, 25);
-		
+
 		JButton btnOdjava = new JButton("Odjava");
 		btnOdjava.setBounds(598, 13, 97, 25);
 		frame.getContentPane().add(btnOdjava);
 	}
 }
-
