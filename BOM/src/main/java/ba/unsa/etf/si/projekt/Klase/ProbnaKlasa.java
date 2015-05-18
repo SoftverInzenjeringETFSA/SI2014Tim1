0762b455f9dd5e231458cb5af67324948b04743a
Zlatan Cilic <zlatancilic@hotmail.com>
2015-05-18 20:08:04 +0200

    radi hibernate

--- BOM/src/main/java/ba/unsa/etf/si/projekt/Klase/ProbnaKlasa.java
+++ BOM/src/main/java/ba/unsa/etf/si/projekt/Klase/ProbnaKlasa.java
@@ -1,6 +1,30 @@
 package ba.unsa.etf.si.projekt.Klase;
 
-// Komentar
-public class ProbnaKlasa {
+import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+
+// Komentar
+@Entity
+public class ProbnaKlasa {
+	@Id
+	@GeneratedValue
+	private long id;
+	private String naziv;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNaziv() {
+		return naziv;
+	}
+	public void setNaziv(String naziv) {
+		this.naziv = naziv;
+	}
 }
