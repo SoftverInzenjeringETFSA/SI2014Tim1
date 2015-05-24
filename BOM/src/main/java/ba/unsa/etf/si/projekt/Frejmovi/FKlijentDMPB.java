98c01cb01d366c4164b4aa01841207a3c8a474f7
ArnelaBesic <arnela06@gmail.com>
2015-05-24 09:26:08 +0200

    FKlijentDMPB radi

--- BOM/src/main/java/ba/unsa/etf/si/projekt/Frejmovi/FKlijentDMPB.java
+++ BOM/src/main/java/ba/unsa/etf/si/projekt/Frejmovi/FKlijentDMPB.java
@@ -23,6 +23,7 @@ import javax.swing.text.MaskFormatter;
 
 import ba.unsa.etf.si.projekt.Klase.Narudzbenica;
 import ba.unsa.etf.si.projekt.Klase.Osoba;
+import ba.unsa.etf.si.projekt.Klase.TipOsobe;
 import ba.unsa.etf.si.projekt.Validacija.Validator;
 import ba.unsa.etf.si.projekt.ServisnaImplementacija.KompanijaFacade;
 import ba.unsa.etf.si.projekt.Klase.Klijent;
@@ -39,21 +40,24 @@ public class FKlijentDMPB {
 	private JButton btnKreiraj;
 	private JPanel panel;
 	private JFrame parentFrame;
- 
+	public Klijent klijent;
+
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
-				
-				//ovaj try sluzi za postavljanje izgleda aplikacije slicnijeg platformi
-				try { 
-				    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+
+				// ovaj try sluzi za postavljanje izgleda aplikacije slicnijeg
+				// platformi
+				try {
+					UIManager.setLookAndFeel(UIManager
+							.getSystemLookAndFeelClassName());
 				} catch (Exception e) {
-				    e.printStackTrace();
+					e.printStackTrace();
 				}
-				
+
 				try {
 					FKlijentDMPB window = new FKlijentDMPB();
 					window.frame.setVisible(true);
@@ -63,78 +67,72 @@ public class FKlijentDMPB {
 			}
 		});
 	}
-	
-	//String klasa je sada stavljen samo beze
-		//umjesto ovoga treba biti odg. klasa
-		public void setFrame(JFrame parentF, String akcijaA, String klasa)
-		{
-			//mozda neka provjera da li je akcija validna (moze i enumeracija)
-			//ali i ne mora :D
-			akcija = akcijaA;
-			
-			
-			//kreiranje, modifikovanje, brisanje, pregled
-			//po defaultu je sve editabilno
-			
-			//za brisanje i pregleda ne trebaju biti editabilini!
-			if(akcija.equals("Brisanje") || akcija.equals("Pregled"))
-			{
-				txtFdsfd.setEditable(false);
-				textField_1.setEditable(false);
-				textField_3.setEditable(false);
-				textField_4.setEditable(false);
-			}
-			//tekst button-a
-			if(akcija.equals("Kreiranje"))
-			{
-				btnKreiraj.setText("Kreiraj");
-			}
-			else if(akcija.equals("Modifikovanje"))
-			{
-				btnKreiraj.setText("Modifikuj");
-			}
-			else if(akcija.equals("Brisanje"))
-			{
-				btnKreiraj.setText("Obriši");
-			}
-			else if(akcija.equals("Pregled"))
-			{
-				btnKreiraj.setText("Nazad");
-			}
-				
-				
-			panel.setBorder(BorderFactory.createTitledBorder(akcija + " klijenta"));
-			parentFrame = parentF;
-			parentFrame.setEnabled(false);
-			frame.setVisible(true);
+
+	public void setFrame(JFrame parentF, String akcijaA, Klijent k) {
+
+		akcija = akcijaA;
+		if (akcija.equals("Brisanje") || akcija.equals("Pregled")) {
+			txtFdsfd.setEditable(false);
+			textField_1.setEditable(false);
+			textField_3.setEditable(false);
+			textField_4.setEditable(false);
+			telefon.setEditable(false);
+			popuniPodatke();
 		}
+
+		if (akcija.equals("Kreiranje")) {
+			btnKreiraj.setText("Kreiraj");
+		} else if (akcija.equals("Modifikovanje")) {
+			btnKreiraj.setText("Modifikuj");
+			popuniPodatke();
+		} else if (akcija.equals("Brisanje")) {
+			btnKreiraj.setText("Obriši");
+		} else if (akcija.equals("Pregled")) {
+			btnKreiraj.setText("Nazad");
+		}
+
+		panel.setBorder(BorderFactory.createTitledBorder(akcija + " klijenta"));
+		parentFrame = parentF;
+		parentFrame.setEnabled(false);
+		frame.setVisible(true);
+	}
 
 	/**
 	 * Create the application.
 	 */
-		InputVerifier imeVal;
-		InputVerifier prezimeVal;
-		InputVerifier adresaVal;
-		InputVerifier emailVal;
-		InputVerifier telefonVal;
+	InputVerifier imeVal;
+	InputVerifier prezimeVal;
+	InputVerifier adresaVal;
+	InputVerifier emailVal;
+	InputVerifier telefonVal;
+
 	public FKlijentDMPB() {
 		initialize();
-		
+		klijent = new Klijent();
+		KompanijaFacade kf = new KompanijaFacade();
+		klijent = (Klijent) kf.returnByImePrezime("klijentt", "klijent",
+				TipOsobe.klijent);
+
 		frame.addWindowListener(new java.awt.event.WindowAdapter() {
-		    @Override
-		    public void windowClosing(java.awt.event.WindowEvent windowEvent) {
-		    	
-		    	parentFrame.setEnabled(true);
-		    	parentFrame.setVisible(true);
-		    	
-		    }
+			@Override
+			public void windowClosing(java.awt.event.WindowEvent windowEvent) {
+
+				parentFrame.setEnabled(true);
+				parentFrame.setVisible(true);
+
+			}
 		});
-		
-		imeVal = new Validator(frame,txtFdsfd,"Molimo unesite ispravno ime","");
-		prezimeVal=new Validator(frame,textField_1,"Molimo unesite ispravno prezime","");
-		adresaVal=new Validator(frame,textField_3,"Adresa koju ste unijeli nije ispravna","adresa");
-		emailVal=new Validator(frame,textField_4,"Email adresa mora da sadrži @ te ispravnu domenu","email");
-		telefonVal=new Validator(frame, telefon, "Molimo unesite telefon","telefon");
+
+		imeVal = new Validator(frame, txtFdsfd, "Molimo unesite ispravno ime",
+				"");
+		prezimeVal = new Validator(frame, textField_1,
+				"Molimo unesite ispravno prezime", "");
+		adresaVal = new Validator(frame, textField_3,
+				"Adresa koju ste unijeli nije ispravna", "adresa");
+		emailVal = new Validator(frame, textField_4,
+				"Email adresa mora da sadrži @ te ispravnu domenu", "email");
+		telefonVal = new Validator(frame, telefon, "Molimo unesite telefon",
+				"telefon");
 		txtFdsfd.setInputVerifier(imeVal);
 		textField_1.setInputVerifier(prezimeVal);
 		textField_3.setInputVerifier(adresaVal);
@@ -151,142 +149,156 @@ public class FKlijentDMPB {
 		frame.setBounds(100, 100, 402, 311);
 		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-		
+
 		panel = new JPanel();
 		panel.setBounds(12, 13, 372, 253);
 		panel.setBorder(BorderFactory.createTitledBorder("Brisanje klijenta"));
 		frame.getContentPane().add(panel);
 		panel.setLayout(null);
-		
+
 		JLabel lblIme = new JLabel("Ime:");
 		lblIme.setHorizontalAlignment(SwingConstants.RIGHT);
 		lblIme.setBounds(12, 50, 109, 16);
 		panel.add(lblIme);
-		
+
 		txtFdsfd = new JTextField();
 		txtFdsfd.setBounds(133, 47, 187, 22);
 		panel.add(txtFdsfd);
 		txtFdsfd.setColumns(10);
-		
+
 		JLabel lblPrezime = new JLabel("Prezime:");
 		lblPrezime.setHorizontalAlignment(SwingConstants.RIGHT);
 		lblPrezime.setBounds(12, 79, 109, 16);
 		panel.add(lblPrezime);
-		
+
 		textField_1 = new JTextField();
 		textField_1.setBounds(133, 76, 187, 22);
 		panel.add(textField_1);
 		textField_1.setColumns(10);
-		
+
 		JLabel lblTelefon = new JLabel("Telefon:");
 		lblTelefon.setHorizontalAlignment(SwingConstants.RIGHT);
 		lblTelefon.setBounds(12, 108, 109, 16);
 		panel.add(lblTelefon);
-		
+
 		JLabel lblAdresa = new JLabel("Adresa:");
 		lblAdresa.setHorizontalAlignment(SwingConstants.RIGHT);
 		lblAdresa.setBounds(12, 137, 109, 16);
 		panel.add(lblAdresa);
-		
+
 		JLabel lblEmail = new JLabel("E-mail:");
 		lblEmail.setHorizontalAlignment(SwingConstants.RIGHT);
 		lblEmail.setBounds(12, 166, 109, 16);
 		panel.add(lblEmail);
-		
+
 		textField_3 = new JTextField();
 		textField_3.setBounds(133, 134, 187, 22);
 		panel.add(textField_3);
 		textField_3.setColumns(10);
-		
+
 		textField_4 = new JTextField();
 		textField_4.setBounds(133, 163, 187, 22);
 		panel.add(textField_4);
 		textField_4.setColumns(10);
-		
+
 		btnKreiraj = new JButton("Obriši");
 		btnKreiraj.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
-				//akcija za klik na dugme koje moze imati razlicite f-je
-				//brisanje, kreiranje, modifikovanje, pregled
-				KompanijaFacade kf=new KompanijaFacade();
-					
-				Boolean da=true;
-				if(txtFdsfd.getText().equals("")|| textField_1.getText().equals("") || telefon.getText().equals("") || textField_4.getText().equals("") || textField_3.getText().equals(""))
-					da=false;
-				if(akcija.equals("Kreiranje"))
-				{
-						
-						String ime=((JTextField)txtFdsfd).getText();
-						String prezime=((JTextField)textField_1).getText();
-						String adresa=((JTextField)textField_3).getText();
-						String email=((JTextField)textField_4).getText();
-						String brojTelefona=((JFormattedTextField)telefon).getText();
-						List<Narudzbenica> narudzbe=new ArrayList<Narudzbenica>();
-						if(da)
-						{
-							if(kf.dodajKlijenta(ime,prezime,brojTelefona,adresa, email, narudzbe))
-								MessageBox.infoBox(frame, "Klijent je uspješno kreiran.", "Info");
-				       }
-						else MessageBox.infoBox(frame, "Molimo unesite sve podatke.", "Greska");
+
+				KompanijaFacade kf = new KompanijaFacade();
+
+				if (akcija.equals("Kreiranje")) {
+
+					String ime = ((JTextField) txtFdsfd).getText();
+					String prezime = ((JTextField) textField_1).getText();
+					String adresa = ((JTextField) textField_3).getText();
+					String email = ((JTextField) textField_4).getText();
+					String brojTelefona = ((JFormattedTextField) telefon)
+							.getText();
+					List<Narudzbenica> narudzbe = new ArrayList<Narudzbenica>();
+					if (validacijaOnClick()) {
+						if (kf.dodajKlijenta(ime, prezime, brojTelefona,
+								adresa, email, narudzbe))
+							MessageBox.infoBox(frame,
+									"Klijent je uspješno kreiran.", "Info");
+					} else
+						MessageBox.infoBox(frame,
+								"Molimo unesite sve podatke.", "Greska");
 				}
-					if(akcija.equals("Modifikovanje")){
-					//	tabela
-				    //treba da iz tabela pokupi klijenta pa njega obrise
-					//	Klijent k=new Klijent(ime,prezime,brojTelefona,adresa, email, narudzbe);
-					 //  kf.mijenjajKlijenta(k);
-						  MessageBox.infoBox(frame, "Podaci o klijentu su uspješno izmijenjeni", "Info");
-						}
-					//ovdje treba procitati formu i
-					//azurirati bazu npr
-					//ispisati messageBox ?
-					//vratiti se nazad
-				
-				if(akcija.equals("Brisanje"))
-				{
-					//treba da iz tabela pokupi klijenta pa njega obrise
-					//Klijent k=
-					//  kf.obrisiOsobu(k);
-					  MessageBox.infoBox(frame, "Klijent je uspješno obrisan", "Info");
-					//ispisati dialogBox ? (da zelite brisati)
-					//azurirati bazu
-					//vratiti se nazad
+				if (akcija.equals("Modifikovanje")) {
+
+					klijent.setIme(((JTextField) txtFdsfd).getText());
+					klijent.setPrezime(((JTextField) textField_1).getText());
+					klijent.setAdresa(((JTextField) textField_3).getText());
+					klijent.setEmail(((JTextField) textField_4).getText());
+					klijent.setBrojTelefona(((JFormattedTextField) telefon)
+							.getText());
+					if (validacijaOnClick()) {
+						kf.mijenjajKlijenta(klijent);
+						MessageBox.infoBox(frame,
+								"Podaci o klijentu su uspješno izmijenjeni",
+								"Info");
+					} else
+						MessageBox.infoBox(frame,
+								"Molimo unesite sve podatke.", "Greska");
 				}
-				if(akcija.equals("Pregled"))
-				{
-					//vratiti se nazad
+
+				if (akcija.equals("Brisanje")) {
+					int dialogButton = JOptionPane.YES_NO_OPTION;
+					int dialogResult = JOptionPane
+							.showConfirmDialog(
+									null,
+									"Da li ste sigurni da želite obrisati ovog klijenat?",
+									"Warning", dialogButton);
+					if (dialogResult == JOptionPane.YES_OPTION) {
+						kf.obrisiOsobu(klijent);
+						MessageBox.infoBox(frame,
+								"Klijent je uspješno obrisan", "Info");
+					}
 				}
-				
+				if (akcija.equals("Pregled")) {
+					frame.dispose();
+					parentFrame.setEnabled(true);
+					parentFrame.setVisible(true);
+				}
+
 			}
 		});
 		btnKreiraj.setBounds(244, 204, 76, 22);
 		panel.add(btnKreiraj);
 		MaskFormatter mf1 = new MaskFormatter();
-		try{
+		try {
 			mf1 = new MaskFormatter("###-###-###");
-		}
-		catch( Exception e)
-		{
-			
+		} catch (Exception e) {
+
 		}
 		mf1.setPlaceholderCharacter('_');
 		telefon = new JFormattedTextField(mf1);
 		telefon.setBounds(133, 106, 187, 20);
 		panel.add(telefon);
-		
-		JButton btnNewButton = new JButton("Poništi");
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				txtFdsfd.setText("");
-				textField_1.setText("");
-				textField_3.setText("");
-				telefon.setText("");
-				textField_4.setText("");
-			}
-		});
-		btnNewButton.setBounds(155, 204, 80, 22);
-		panel.add(btnNewButton);
-		
-		
+
+	}
+
+	public void popuniPodatke() {
+		txtFdsfd.setText(klijent.getIme());
+		textField_1.setText(klijent.getPrezime());
+		textField_3.setText(klijent.getAdresa());
+		textField_4.setText(klijent.getEmail());
+		telefon.setText(klijent.getBrojTelefona());
+	}
+
+	public boolean validacijaOnClick() {
+
+		textField_1.setInputVerifier(prezimeVal);
+		textField_3.setInputVerifier(adresaVal);
+		textField_4.setInputVerifier(emailVal);
+		telefon.setInputVerifier(telefonVal);
+		Boolean da = true;
+		if (txtFdsfd.getText().equals("") || textField_1.getText().equals("")
+				|| telefon.getText().equals("")
+				|| textField_4.getText().equals("")
+				|| textField_3.getText().equals(""))
+			da = false;
+		return da;
 	}
 }
