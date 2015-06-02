9071dc99065c2bdfed0752b638400edc70a4544e
Zlatan Cilic <zlatancilic@hotmail.com>
2015-06-02 16:57:52 +0200

    remote access, NE DIRAJ NISTA

--- BOM/src/main/java/ba/unsa/etf/si/projekt/Baza/ProbnaKlasaZlatan.java
+++ BOM/src/main/java/ba/unsa/etf/si/projekt/Baza/ProbnaKlasaZlatan.java
@@ -4,6 +4,8 @@ import java.util.List;
 import ba.unsa.etf.si.projekt.Klase.Materijal;
 import ba.unsa.etf.si.projekt.Klase.Menadzer;
 import ba.unsa.etf.si.projekt.Klase.Osoba;
+import ba.unsa.etf.si.projekt.Klase.Ovlasti;
+import ba.unsa.etf.si.projekt.Klase.Radnik;
 import ba.unsa.etf.si.projekt.Klase.Sastavnica;
 import ba.unsa.etf.si.projekt.Klase.TipOsobe;
 import ba.unsa.etf.si.projekt.ServisnaImplementacija.KompanijaFacade;
@@ -14,8 +16,20 @@ public class ProbnaKlasaZlatan {
 		KompanijaFacade kf = new KompanijaFacade();
 		SkladisteFacade sf = new SkladisteFacade();
 		Sastavnica ls = sf.pretragaSastavnica("SAS001");
-		if(ls != null)
-			System.out.println(sf.obrišiSastavnicu(ls));
+		Radnik r = new Radnik();
+		r.setAdresa("asadad");
+		r.setBrojTelefona("565656565");
+		r.setEmail("sdda@dsd.ds");
+		r.setIme("Vatko");
+		r.setPrezime("Debeli");
+		r.setJMBG("2006993150008");
+		r.setNivoOvlasti(null);
+		r.setTipOsobe(TipOsobe.radnik);
+		r.setPassword("sdsdsdsdsd");
+		r.setUsername("dbvatko");
+		
+		
+		kf.dodajRadnika(r.getIme(), r.getPrezime(), r.getBrojTelefona(), r.getAdresa(), r.getEmail(), r.getPozicija(), Ovlasti.brisanjeMaterijala, r.getUsername(), r.getPassword(), r.getJMBG());
 	}
 
 }
